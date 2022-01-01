c1b8662766361a2de3948a23dd3b01e98e55ffaa
NikitaKhapare <NikitaKhapare53@gmail.com>
2021-12-31 22:55:12 -0800

    Adding for loop problems

---
+++ for loop/prime.sh
@@ -0,0 +1,19 @@
+read -p "enter the list of  numbers : "  n
+isPrime=1
+for ((i=2;i<$n;i++))
+do
+ if [ $((n%i)) -eq 0 ]
+  then
+    isPrime=0
+    break
+ fi
+
+done
+
+if [ $isPrime -eq 1 ]
+then
+ echo "it is prime"
+
+else
+ echo "is NOT prime"
+fi
