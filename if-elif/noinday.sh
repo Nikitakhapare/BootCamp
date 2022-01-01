d7c3d02bfcc7443815fbb5a34c297dd5cfae0541
NikitaKhapare <NikitaKhapare53@gmail.com>
2021-12-31 22:55:58 -0800

    Adding if-elif problems

---
+++ if-elif/noinday.sh
@@ -0,0 +1,23 @@
+read -p "Enter a number:= " n
+if [ $n -eq 1 ]
+then 
+echo "It is a Sunday "
+elif [ $n -eq 2 ]
+then
+echo "It is a Monday "
+elif [ $n -eq 3 ]
+then
+echo "It is a tuesday "
+elif [ $n -eq 4 ]
+then 
+echo "It is wednesday "
+elif [ $n -eq 5 ]
+then
+echo "It is a thusday "
+elif [ $n -eq 6 ]
+then 
+echo "It is a Friday "
+elif [ $n -eq 7 ]
+then
+echo "It is a saturday "
+fi
